75b9c88a6269e6fec56a425ac86b8944858d592a
Claire <clairefraser121@gmail.com>
2024-01-06 20:33:29 +0000

    made code DRY

--- starter/index.js
+++ starter/index.js
@@ -1,7 +1,8 @@
+const { createManager, createEngineer, createIntern, getMenuChoice } = require("./src/promptUtils.js");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");  // Use require for CommonJS modules
+const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 const render = require("./src/page-template.js");
@@ -23,108 +24,20 @@ async function startApp() {
     console.log("Output folder already exists.");
   }
 
-// Array to store team members 
-const teamMembers = [];
+   // Loop to gather information about the team members
+   let menuChoice = "";
+   while (menuChoice !== "Finish building the team") {
+       menuChoice = await getMenuChoice();
 
-// Prompt for manager information
-const managerQuestions = [
-    {
-        type: "input",
-        name: "name",
-        message: "Enter the manager's name:",
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Enter the manager's ID:",
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Enter the manager's email:",
-    },
-    {
-        type: "input",
-        name: "officeNumber",
-        message: "Enter the manager's office number:",
-    },
-];
-
-const menuQuestion = {
-    type: "list",
-    name: "menuChoice",
-    message: "What would you like to do next?",
-    choices: ["Add an engineer", "Add an intern", "Finish building the team"],
-};
-
-const engineerQuestions = [
-    {
-        type: "input",
-        name: "name",
-        message: "Enter the engineer's name:",
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Enter the engineer's ID:",
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Enter the engineer's email:",
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "Enter the engineer's GitHub username:",
-    },
-];
-
-const internQuestions = [
-    {
-        type: "input",
-        name: "name",
-        message: "Enter the intern's name:",
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Enter the intern's ID",
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Enter the intern's email:",
-    },
-    {
-        type: "input",
-        name: "school",
-        message: "Enter the intern's school:",
-    },
-];
-
-
-const managerAnswers = await inquirer.prompt(managerQuestions);
-const manager = new Manager(managerAnswers.name, managerAnswers.id, managerAnswers.email, managerAnswers.officeNumber);
-
-
-teamMembers.push(manager);
-
-// Loop to gather information about the team members
-let menuChoice = "";
-while (menuChoice !== "Finish building the team") {
-  const menuAnswer = await inquirer.prompt(menuQuestion);
-  menuChoice = menuAnswer.menuChoice;
-
-  // Process user choice
-  if (menuChoice === "Add an engineer") {
-    const engineerAnswers = await inquirer.prompt(engineerQuestions);
-    teamMembers.push(new Engineer(engineerAnswers.name, engineerAnswers.id, engineerAnswers.email, engineerAnswers.github));
-  } else if (menuChoice === "Add an intern") {
-    const internAnswers = await inquirer.prompt(internQuestions);
-    teamMembers.push(new Intern(internAnswers.name, internAnswers.id, internAnswers.email, internAnswers.school));
-  }
-}
+       // Process user choice
+       if (menuChoice === "Add a manager") {
+           teamMembers.push(await createManager());
+       } else if (menuChoice === "Add an engineer") {
+           teamMembers.push(await createEngineer());
+       } else if (menuChoice === "Add an intern") {
+           teamMembers.push(await createIntern());
+       }
+   }
 
 // Render HTML using the team members
 const renderedHtml = render(teamMembers);
