d691b35057e6bb5b5b9b7b91f16e106604accd3c
Claire <clairefraser121@gmail.com>
2024-01-06 16:07:01 +0000

    added styling

--- starter/src/page-template.js
+++ starter/src/page-template.js
@@ -4,60 +4,63 @@ const generateTeam = team => {
     // creates the manager html
     const generateManager = manager => {
         return `
-            <div class="card m-2 employee-card">
-                <div class="card-header">
-                    <h2 class="card-title">${manager.getName()}</h2>
-                    <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
-                </div>
-                <div class="card-body">
-                    <ul class="list-group">
-                        <li class="list-group-item">ID: ${manager.getId()}</li>
-                        <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                        <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
-                    </ul>
-                </div>
-            </div>
+        <div class="card m-2 employee-card shadow">
+        <div class="card-header" style="background-color: orange; color: white;">
+            <h2 class="card-title">${manager.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2" style="color: rgb(117, 114, 114);"></i>Manager</h3>
+        </div>
+        <div class="card-body" style="background-color: #ffebcc; color: rgb(95, 92, 92);">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${manager.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}" class="text-dark">${manager.getEmail()}</a></li>
+                <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+            </ul>
+        </div>
+    </div>
+    
         `;
     };
 
     // creates the html for engineers
     const generateEngineer = engineer => {
         return `
-            <div class="card m-2 employee-card">
-                <div class="card-header">
-                    <h2 class="card-title">${engineer.getName()}</h2>
-                    <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
-                </div>
-                <div class="card-body">
-                    <ul class="list-group">
-                        <li class="list-group-item">ID: ${engineer.getId()}</li>
-                        <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-                        <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
-                    </ul>
-                </div>
-            </div>
+        <div class="card m-2 employee-card shadow">
+        <div class="card-header" style="background-color: green; color: white;">
+            <h2 class="card-title">${engineer.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-glasses mr-2" style="color: rgb(77, 74, 74);"></i>Engineer</h3>
+        </div>
+        <div class="card-body" style="background-color: #b3ffb3; color: rgb(95, 92, 92);">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${engineer.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}" class="text-dark">${engineer.getEmail()}</a></li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer" class="text-dark">${engineer.getGithub()}</a></li>
+            </ul>
+        </div>
+    </div>
+    
         `;
     };
 
     // creates the html for interns
     const generateIntern = intern => {
         return `
-            <div class="card m-2 employee-card">
-                <div class="card-header">
-                    <h2 class="card-title">${intern.getName()}</h2>
-                    <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
-                </div>
-                <div class="card-body">
-                    <ul class="list-group">
-                        <li class="list-group-item">ID: ${intern.getId()}</li>
-                        <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-                        <li class="list-group-item">School: ${intern.getSchool()}</li>
-                    </ul>
-                </div>
-            </div>
+        <div class="card m-2 employee-card shadow">
+        <div class="card-header" style="background-color: teal; color: white;">
+            <h2 class="card-title">${intern.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-user-graduate mr-2" style="color: rgb(57, 55, 55);;"></i>Intern</h3>
+        </div>
+        <div class="card-body" style="background-color: #99e6e6; color: rgb(95, 92, 92);">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${intern.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}" class="text-dark">${intern.getEmail()}</a></li>
+                <li class="list-group-item">School: ${intern.getSchool()}</li>
+            </ul>
+        </div>
+    </div>
         `;
     };
-    
+
+
     const html = [];
 
     html.push(team
@@ -97,32 +100,32 @@ module.exports = team => {
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
 </head>
 
-<body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">My Team</h1>
-            </div>
+<div class="container-fluid my-team-background p-4">
+    <div class="row">
+    <div class="col-12 jumbotron mb-3 team-heading" style="background-color: #b3bcf5; color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);">
+            <h1 class="text-center display-4 font-weight-bold">My Team</h1>
         </div>
     </div>
-    <div class="container">
-        <div class="row">
-            <div class="team-area col-12 d-flex justify-content-center">
-                ${generateTeam(team)}
-            </div>
+</div>
+<div class="container-fluid p-4">
+    <div class="row">
+        <div class="team-area col-12 d-flex justify-content-center">
+            ${generateTeam(team)}
         </div>
     </div>
-    <!-- Bootstrap JS and Popper.js scripts -->
+</div>
+
+
+    <!-- Bootstrap JS and Popper.js -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
+
 </html>
+
     `;
 };
